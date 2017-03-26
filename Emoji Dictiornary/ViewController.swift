4dd388020a9a349af2b2c5f7f1dd2197d4f2f01b
Chicco <3nomemio@gmail.com>
2017-03-26 18:51:48 +0200

    Initial Commit

---
+++ Emoji Dictiornary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji Dictiornary
+//
+//  Created by Chicco on 26/03/2017.
+//  Copyright © 2017 ArduJack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
